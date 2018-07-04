026ede9d5649f1314384f56047f9b26bc44edac0
Ryanc_signiq <ryanc@signiq.com>
2018-07-04 17:01:05 +0800

    Added testing for execute and prepare procs

--- src/sqlnim.nim
+++ src/sqlnim.nim
@@ -7,8 +7,8 @@ import sqlnim/table, sqlnim/row, sqlnim/shared
 type
     MetaCommandResult = enum
         META_COMMAND_SUCCESS,
-        META_COMMAND_UNRECOGNIZED_COMMAND
-
+        META_COMMAND_UNRECOGNIZED_COMMAND,
+        META_COMMAND_QUIT
 
     PrepareResult = enum
         PREPARE_SUCCESS = "Success",
@@ -21,7 +21,6 @@ proc writeVersion()
 proc doMetaCommand(input: string): MetaCommandResult
 proc prepareStatement(input: string, statement: var Statement): PrepareResult
 proc execute_statement(statement: Statement, table: Table): ExecuteResult
-
 
 proc rowCols(): uint =
     const r:Row = Row()
@@ -50,10 +49,10 @@ proc main(parser: var OptParser) =
             case key
             of "help", "h": 
                 writeHelp()
-                quit(QUITSUCCESS)
+                return
             of "version", "v": 
                 writeVersion()
-                quit(QUITSUCCESS)
+                return
         of cmdEnd: assert(false)
 
     var secondLine = false
@@ -68,6 +67,8 @@ proc main(parser: var OptParser) =
             of META_COMMAND_UNRECOGNIZED_COMMAND:
                 stderr.writeLine "Unrecognized command: " & input_line
                 continue
+            of META_COMMAND_QUIT:
+                return
         
         case prepareStatement(input_line, statement)
         of PREPARE_SUCCESS:
@@ -93,7 +94,7 @@ proc writeVersion() =
 
 proc doMetaCommand(input: string): MetaCommandResult =
     if cmpIgnoreCase(input, ".exit") == 0:
-        quit(QUITSUCCESS)
+        return META_COMMAND_QUIT
     elif cmpIgnoreCase(input, ".help") == 0:
         echo "SQLNim: Available commands are:"
         echo ".exit"
@@ -108,10 +109,13 @@ proc prepareStatement(input: string, statement: var Statement): PrepareResult =
     ]##
     var 
         id: int
-        username, email: cstring
+        username, email: string
         args = input.split(' ')
 
-    id = parseInt(args[1])
+    try:
+        id = parseInt(args[1])
+    except ValueError:
+        return PREPARE_SYNTAX_ERROR
 
     if args[0].cmpIgnoreCase("insert") == 0:
         statement.type = STATEMENT_INSERT
@@ -139,68 +143,142 @@ proc execute_statement(statement: Statement, table: Table): ExecuteResult =
 proc init() =
     var parser = initOptParser()
     main(parser)
+    quit(QUITSUCCESS)
 
 when isMainModule:
-    import unittest, os, ospaths, streams, memfiles
-    from posix import dup, dup2
+    import unittest, os, ospaths, streams
+    from posix import dup, dup2, onSignal, SIGHUP
+
+    proc run(): int =
+        ## Test friendly version of init
+        var parser = initOptParser()
+        main(parser)
+        return QUITSUCCESS
 
     suite "Application Tests":
 
-        setup:
-            echo "Starting Test"
+        test "Application can insert a row":
             let 
                 test_dir = getCurrentDir() / "tests/fixtures/"
+
+            var
                 saved_stdout = dup(1)
                 saved_stdin = dup(0)
-            var
-                test_stdout: MemFile
-
-        teardown:
-            close(stdin)
-            discard dup2(saved_stdin, 0) 
-            
-        test "Application can insert a row":
-            var 
+                saved_stderr = dup(2)
                 in_test_filename = test_dir / "insert_testdata"
                 out_test_filename = test_dir / "out_testdata"
-                redir_stdin, redir_stdout: File
+                redir_stdin, redir_stdout, null_stderr: File
+
+            stdin.flushFile
+            stdout.flushFile
+            stderr.flushFile
                 
+            redir_stdin = open(in_test_filename, fmRead)
+            redir_stdout = open(out_test_filename, fmWrite)
+            null_stderr = open("/dev/null", fmWrite)
 
-
-            redir_stdin = system.open(in_test_filename, fmRead)
-            redir_stdout = system.open(out_test_filename, fmWrite)
             #Duplicate our stdin
-            stderr.writeLine($getFileInfo(stdout).id.file)
-            discard dup2(getFileHandle(redir_stdin), 0)
-            discard dup2(getFileHandle(redir_stdout), 1)
-            stderr.writeLine($getFileInfo(stdout).id.file)
+            discard dup2(redir_stdin.getFileHandle, 0)
+            discard dup2(redir_stdout.getFileHandle, 1)
+            discard dup2(null_stderr.getFileHandle, 2)
+            redir_stdout.close
+            redir_stdin.close
+            null_stderr.close
 
             checkpoint("Passing in STDIN")
             
-            init()
+            var result = run()
+            check(result == QUITSUCCESS)
+            stdout.flushFile
 
-            close(stdout)
-            close(redir_stdout)
-            discard dup2(saved_stdout, 1)
-            stderr.writeLine($getFileInfo(stdout).id.file)
+            discard dup2(saved_stdout, stdout.getFileHandle)
+            discard dup2(saved_stdin, stdin.getFileHandle)
+            discard dup2(saved_stderr, stderr.getFileHandle)
+
+            redir_stdout = open(out_test_filename)
+
             checkpoint("Reading results")
             var success_counts = 0
             for line in redir_stdout.lines:
-                stderr.writeLine(line)
                 if "Executed successfully" in line:
                     success_counts.inc
+            redir_stdout.close
 
-
-
-            redir_stdin.setFilePos(0)
+            redir_stdin = open(in_test_filename)
             var total_in_lines = 0
             for line in redir_stdin.lines:
-                total_in_lines.inc
+                if line != ".exit":
+                    total_in_lines.inc
+            redir_stdin.close
 
-            check success_counts == total_in_lines
+            check(success_counts == total_in_lines)
 
-        test "Application can select a row":
-            echo "Test 2"
+        test "Test Meta Commands":
+            var 
+                exit = ".exit"
+                help = ".help"
+                fake = ".fake"
+                meta_result: MetaCommandResult
 
-        test "Application can insert, then select a row":
-            echo "Test 3"
+            checkpoint("Testing .help")
+            meta_result = doMetaCommand(help)
+            check(meta_result == META_COMMAND_SUCCESS)
+
+            checkpoint("Testing .exit")
+            meta_result = doMetaCommand(exit)
+            check(meta_result == META_COMMAND_QUIT)
+
+            checkpoint("Testing .fake")
+            meta_result = doMetaCommand(fake)
+            check(meta_result == META_COMMAND_UNRECOGNIZED_COMMAND)
+
+        test "Test PrepareStatement":
+            var 
+                p_result: PrepareResult
+                statement: Statement = new Statement
+                insert_statement = "insert 1 a a@a.a"
+                select_statement = "select 1"
+                invalid_statement = "select a a v d"
+                unrecognized_statement = "anothercommand 1"
+
+
+            checkpoint("Testing insert statement")
+            p_result = prepareStatement(insert_statement, statement)
+            check(p_result == PREPARE_SUCCESS)
+            check(statement.type == STATEMENT_INSERT)
+
+            checkpoint("Testing select statement")
+            p_result = prepareStatement(select_statement, statement)
+            check(p_result == PREPARE_SUCCESS)
+            check(statement.type == STATEMENT_SELECT)
+
+            checkpoint("Testing invalid statement")
+            p_result = prepareStatement(invalid_statement, statement)
+            check(p_result == PREPARE_SYNTAX_ERROR)
+            check(statement.type == STATEMENT_SELECT)
+
+            checkpoint("Testing unrecognized statement")
+            p_result = prepareStatement(unrecognized_statement, statement)
+            check(p_result == PREPARE_UNRECOGNIZED_STATEMENT)
+            check(statement.type == STATEMENT_SELECT)
+
+        test "Test ExecuteResult":
+            var
+                table = new_table()
+                insert_statement, select_statement: Statement = new Statement
+                e_result: ExecuteResult
+                username = "Test"
+                email = "Test@test.com"
+
+            checkpoint("Testing INSERT statement")
+            insert_statement.type = STATEMENT_INSERT
+            insert_statement.row_to_insert = new_row(id = 1, username = username, email = email)
+
+            e_result = execute_statement(insert_statement, table)
+            check(e_result == EXECUTE_SUCCESS)
+
+            select_statement.type = STATEMENT_SELECT
+
+            e_result = execute_statement(select_statement, table)
+            check(e_result == EXECUTE_SUCCESS)
+
